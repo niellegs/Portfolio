ca8a2fca8f964d1b8d4e89571624f756b47d3c5d
LYNX <contato.daniellegsantos@gmail.com>
2025-02-06 12:53:17 -0300

    add - skill

--- portfolio/src/Components/Skills/Skills.tsx
+++ portfolio/src/Components/Skills/Skills.tsx
@@ -3,6 +3,7 @@ import "./Skills.css";
 
 function Skills() {
     const topics: string[] = ["Front-End", "Back-End", "Mobile", "Outros"];
+
     interface Skill {
         topic: string;
         skill: string;
@@ -10,27 +11,27 @@ function Skills() {
         percentage: number;
         src: string;
     }
+
     const skills: Skill[] = [
         { topic: "Front-End", skill: "HTML", description: "HTML é uma linguagem utilizada para dar corpo às páginas web.", percentage: 90, src: "fa-brands fa-html5" },
         { topic: "Front-End", skill: "CSS", description: "Cascading Style Sheets é um mecanismo para adicionar estilos (cores, fontes, espaçamento, etc.) a uma página web.", percentage: 90, src: "fa-brands fa-css3" },
         { topic: "Front-End", skill: "Javascript", description: "(às vezes abreviado para JS) é uma linguagem leve, interpretada e baseada em objetos com funções de primeira classe, mais conhecida como a linguagem de script para páginas Web, mas usada também em vários outros ambientes sem browser", percentage: 70, src: "fa-brands fa-js" }
     ];
 
-    const [selectedTopic, setsSelectedTopic] = useState<string | null>(null);
-    const [selectedSkill, setSelectedSkill] = useState<Skill | null>(null); // Novo estado para a skill selecionada
+    const [selectedTopic, setSelectedTopic] = useState<string | null>(null);
+    const [selectedSkill, setSelectedSkill] = useState<Skill | null>(null);
 
     const handleSkillClick = (topic: string) => {
-        setsSelectedTopic(topic === selectedTopic ? null : topic);
-        setSelectedSkill(null); // Limpa a skill selecionada ao mudar de tópico
+        setSelectedTopic(topic === selectedTopic ? null : topic);
+        setSelectedSkill(null);
     };
 
     const handleSkillSelect = (skill: Skill) => {
-        setSelectedSkill(skill); // Atualiza a skill selecionada
+        setSelectedSkill(skill);
     };
 
     return (
         <div className="skill_container">
-
             <div className="skill_menu">
                 {topics.map((topic) => (
                     <button key={topic} onClick={() => handleSkillClick(topic)}>
@@ -39,34 +40,35 @@ function Skills() {
                 ))}
             </div>
 
-            <div className="skill_list display-flex">
-                {/* Exibe informações da skill selecionada */}
-                {selectedSkill && (
-                    <div className="skill_info">
-                        <h2 className="title">{selectedSkill.skill}</h2>
-                        <p>{selectedSkill.description}</p>
-                        <p className="display-flex">
-                            {selectedSkill.percentage}% 
-                            <div className="percentage" style={{ width: `${selectedSkill.percentage}%` }}></div>
-                        </p>
-                    </div>
-                )}
+            <div className="skill_list display-flex justify-center">
+                <div className="skill_info">
+                    {selectedSkill ? (
+                        <>
+                            <h2 className="title">{selectedSkill.skill}</h2>
+                            <p>{selectedSkill.description}</p>
+                            <p className="display-flex">
+                                {selectedSkill.percentage}%
+                                <div className="percentage" style={{ width: `${selectedSkill.percentage}%` }}></div>
+                            </p>
+                        </>
+                    ) : (
+                        <p>Selecione uma habilidade para ver os detalhes</p>
+                    )}
+                </div>
 
-                {selectedTopic && (
-                    <ul className="display-flex">
-                        {skills
-                            .filter((skill) => skill.topic === selectedTopic)
-                            .map((skill) => (
-                                <li 
-                                    className={skill.skill} 
-                                    key={skill.skill} 
-                                    onClick={() => handleSkillSelect(skill)} // Atualiza a skill selecionada ao clicar
-                                >
-                                    <i className={skill.src}></i>
-                                </li>
-                            ))}
-                    </ul>
-                )}
+                <ul className="display-flex skill">
+                    {skills
+                        .filter((skill) => selectedTopic ? skill.topic === selectedTopic : true)
+                        .map((skill) => (
+                            <li 
+                                className={skill.skill} 
+                                key={skill.skill} 
+                                onClick={() => handleSkillSelect(skill)}
+                            >
+                                <i className={skill.src}></i>
+                            </li>
+                        ))}
+                </ul>
             </div>
         </div>
     );
